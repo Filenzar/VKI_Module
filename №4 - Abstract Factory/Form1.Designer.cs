0a2e8862e70238605d0b2fe03c1f48f38169ce46
Filenzar <160037720+Filenzar@users.noreply.github.com>
2024-10-02 19:08:57 +0700

    Add files via upload

---
+++ №4 - Abstract Factory/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Abstract_Factory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Location = new System.Drawing.Point(12, 254);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(124, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 224);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(124, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(148, 344);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ №4 - Abstract Factory/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace Abstract_Factory
+{
+    public partial class Form1 : Form
+    {
+        private IShapeFactory currentFactory;
+        private IShape selectedShape = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("Red Factory");
+            comboBox1.Items.Add("Blue Factory");
+            comboBox1.SelectedIndexChanged += OnFactoryChanged;
+
+
+            comboBox2.Items.Add("Circle");
+            comboBox2.Items.Add("Square");
+            comboBox2.Items.Add("Triangle");
+            comboBox2.SelectedIndexChanged += OnShapeChanged;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 0;
+        }
+        private void OnFactoryChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    currentFactory = new RedFactory();
+                    break;
+                case 1:
+                    currentFactory = new BlueFactory();
+                    break;
+            }
+            if (selectedShape != null)OnShapeChanged(sender, e);
+
+            Invalidate();
+        }
+        private void OnShapeChanged(object sender, EventArgs e)
+        {
+
+
+            if (comboBox2.SelectedItem.ToString() == "Circle")
+            {
+                selectedShape = currentFactory.CreateCircle();
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Square")
+            {
+                selectedShape = currentFactory.CreateSquare();
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Triangle")
+            {
+                selectedShape = currentFactory.CreateTriangle();
+            }
+            else
+            {
+                throw new InvalidOperationException("Unknown shape");
+            }
+
+            Invalidate();
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (selectedShape!= null) 
+            {
+                selectedShape.Draw(e.Graphics);
+            }
+        }
+    }
+
+    public interface IShape
+    {
+        void Draw(Graphics graphics);
+    }
+
+    public class Circle : IShape
+    {
+        private Color color;
+
+        public Circle(Color color)
+        {
+            this.color = color;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            using (Brush brush = new SolidBrush(color))
+            {
+                graphics.FillEllipse(brush, 10, 10, 100, 100);
+            }
+        }
+    }
+
+    public class Square : IShape
+    {
+        private Color color;
+
+        public Square(Color color)
+        {
+            this.color = color;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            using (Brush brush = new SolidBrush(color))
+            {
+                graphics.FillRectangle(brush, 10, 10, 100, 100);
+            }
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        private Color color;
+
+        public Triangle(Color color)
+        {
+            this.color = color;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            using (Brush brush = new SolidBrush(color))
+            {
+                Point[] points = {
+                new Point(60, 10),
+                new Point(10, 110),
+                new Point(110, 110)
+            };
+                graphics.FillPolygon(brush, points);
+            }
+        }
+    }
+    public interface IShapeFactory
+    {
+        IShape CreateCircle();
+        IShape CreateSquare();
+        IShape CreateTriangle();
+    }
+
+    public class RedFactory : IShapeFactory
+    {
+        public IShape CreateCircle() => new Circle(Color.Red);
+        public IShape CreateSquare() => new Square(Color.Red);
+        public IShape CreateTriangle() => new Triangle(Color.Red);
+    }
+
+    public class BlueFactory : IShapeFactory
+    {
+        public IShape CreateCircle() => new Circle(Color.Blue);
+        public IShape CreateSquare() => new Square(Color.Blue);
+        public IShape CreateTriangle() => new Triangle(Color.Blue);
+    }
+
+}
